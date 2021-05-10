03582acabf0b3c60e13556856742df6aa6d5f9ab
Alexey Ivanov <amivanoff@gmail.com>
2021-05-10 10:05:26 +0300

    [CHORE] GH-21 Build, CI, Test fixes

--- test/stories.test.tsx
+++ test/stories.test.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Default as Thing } from '../stories/Thing.stories';
+import { TimeSeries } from '../stories/DataRenderer.stories';
 
-describe('Thing', () => {
+describe('DataRenderer.stories', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<Thing />, div);
+    ReactDOM.render(<TimeSeries />, div);
     ReactDOM.unmountComponentAtNode(div);
   });
 });
