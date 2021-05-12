2e9863107f0bd062c7e8bf8ae2d92ab75c1a5b7b
Alexey Ivanov <amivanoff@gmail.com>
2021-05-12 09:54:45 +0300

    [FIX] GH-16 Story test fails

--- test/stories.test.tsx
+++ test/stories.test.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import * as ReactDOM from 'react-dom';
+import { act, render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+
 import { TimeSeries } from '../stories/DataRenderer.stories';
 
 describe('DataRenderer.stories', () => {
-  it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<TimeSeries />, div);
-    ReactDOM.unmountComponentAtNode(div);
+  it('renders without crashing', async () => {
+    await act(async () => {
+      render(<TimeSeries {...TimeSeries.args} />);
+    });
   });
 });
