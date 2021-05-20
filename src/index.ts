efbde06e478d216c2976cb5247403529580aa02a
Alexey Ivanov <amivanoff@gmail.com>
2021-05-20 22:09:11 +0300

    [REFACTOR] GH-30 Build, storybook, example works

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+export * from './charts/Chart';
+export * from './charts/DataSetDemo';
+export * from './charts/GaugeChart';
+export * from './charts/MultiViewDodgeDemo';
+export * from './charts/PieTooltipDemo';
+export * from './charts/RadarChart';
+
+export * as TimeSeriesWithAuxiliaryView from './charts/TimeSeriesWithAuxiliaryView';
+
+export * from './DataRenderer';
+
+export * as DateRangePickerMenu from './DateRangePickerMenu';
+
+// test data
+export * from './store/RemoteData';
