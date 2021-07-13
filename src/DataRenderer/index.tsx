d2c80eca047ce2bb9778651d12e4fc140cf35bc0
Dmitry Oshkalo <dmitry.oshkalo@gmail.com>
2021-07-13 19:50:03 +0300

    Remove DataRenderer component

--- src/DataRenderer/index.tsx
+++ src/DataRenderer/index.tsx
@@ -29,57 +29,6 @@ function mapToMeta(constraint: any): any {
   };
 }
 
-export const DataRenderer = ({ viewKinds, viewDescriptions, data }: any): JSX.Element => {
-  const [views, setViews] = useState<any>([]);
-
-  useEffect(() => {
-    async function loadViews() {
-      // map viewDescription constraints to G2 views and geometries
-      const mappedViews = viewDescriptions.map(async (viewDescription: any) => {
-        const viewKind = viewKinds.find((vk: any) => vk['@id'] === viewDescription.viewKind);
-        const { mappings = {}, dataMappings = [] } = viewKind;
-        const viewPartMapper = ViewPartMapper(mappings, dataMappings);
-        const viewConfig = viewDescription.elements
-          .map((viewElement: any) => {
-            const elementCollectionConstraint = viewDescription.collsConstrs.find(
-              (constraint: { [x: string]: any }) => constraint['@id'] === viewElement.resultsScope,
-            );
-            const viewElementMeta = elementCollectionConstraint?.entConstrs
-              .map((e: any) => mapToMeta(e))
-              .reduce((acc: any, item: any) => ({ ...acc, ...item }), {});
-
-            return { ...viewElementMeta, ...viewElement };
-          })
-          .map((elementWithMeta: any) => {
-            const viewElementData = data[elementWithMeta.resultsScope]?.dataIntrnl ?? [];
-            const chartViewPart = viewPartMapper.createChartViewPart(elementWithMeta, viewElementData);
-            return chartViewPart;
-          })
-          .reduce(viewPartReducer, createEmptyViewPart());
-        const chartConfig = {
-          title: viewDescription.title,
-          description: viewDescription.description,
-          options: viewDescription.options,
-          views: [viewConfig],
-        };
-        const dataViewComponent = await createComponent(viewKind.type);
-        return { View: dataViewComponent, key: viewDescription['@id'], config: chartConfig };
-      });
-      Promise.all(mappedViews).then(setViews);
-    }
-    loadViews();
-  }, [viewDescriptions, data, viewKinds]);
-
-  return (
-    <React.Suspense fallback={<Spin />}>
-      {views.map((item: { View: any; key: any; config: any }) => {
-        const { View, key, config } = item;
-        return <View key={key} {...config} />;
-      })}
-    </React.Suspense>
-  );
-};
-
 export const ChartRenderer = observer<any>(({ viewDescrObs, viewKindObs }: any): JSX.Element => {
   const { store } = useContext(MstContext);
   const [views, setViews] = useState<any>([]);
