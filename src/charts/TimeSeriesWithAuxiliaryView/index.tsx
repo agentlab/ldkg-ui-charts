3674b35c7d54d24e03f55bbc478a23a4d4a67548
Dmitry Oshkalo <dmitry.oshkalo@gmail.com>
2021-08-02 19:29:41 +0300

    Move header and range picker out of time series chart component

--- src/charts/TimeSeriesWithAuxiliaryView/index.tsx
+++ src/charts/TimeSeriesWithAuxiliaryView/index.tsx
@@ -8,49 +8,13 @@
  * SPDX-License-Identifier: GPL-3.0-only
  ********************************************************************************/
 import { G2, Mix } from '@ant-design/charts';
-import { Datum } from '@antv/g2plot/lib/types';
 import 'antd/dist/antd.css';
-import moment, { Moment } from 'moment';
 import React, { useEffect, useRef, useState } from 'react';
-import DateRangePickerMenu from '../../DateRangePickerMenu';
-import styles from './TimeSeriesWithAuxiliaryView.module.scss';
 import { configureAxesScales, configureYAxes, getXYScales, scaleDataToTimeUnit } from './utils';
 
-const TimeSeriesWithAuxiliaryView = ({ views = {}, options = {}, title, description }: any) => {
-  const [plot, setPlot] = useState<any>(null);
+const TimeSeriesWithAuxiliaryView = ({ views = {}, options = {} }: any) => {
   const [chartConfig, setChartConfig] = useState<any>({});
   const legendItems = useRef<any[]>([]);
-
-  const handleDatesChanged = (dates: [Moment, Moment]) => {
-    const { views: chartViews = [] } = plot?.chart;
-    const { timeUnit = null } = options;
-    chartViews.forEach((view: any) => {
-      view.filter('*', (value: any, data: Datum) => {
-        const resultDate = moment(data.resultTime);
-        return resultDate.isSameOrAfter(dates[0], timeUnit) && resultDate.isSameOrBefore(dates[1], timeUnit);
-      });
-      view.render();
-    });
-  };
-
-  const handleLegendClick = (element: any, legend: any) => {
-    const { views: chartViews = [] } = plot?.chart;
-    chartViews.forEach((view: any) => {
-      view.filter(
-        legend.link,
-        legend.enabled
-          ? (value: any, data: Datum) => {
-              return data[legend.dataField] !== legend.link;
-            }
-          : null,
-      );
-      view.render();
-    });
-    const legendIdx = legendItems.current.findIndex((legendItem: any) => legendItem.link === legend.link);
-    legendItems.current[legendIdx].enabled = !legend.enabled;
-    element.className = legend.enabled ? styles.legendItemActive : styles.legendItem;
-    element.style.borderColor = legend.enabled ? legend.color : '#DADCE0';
-  };
 
   const theme = {
     colors10: ['#FACDAA', '#F4A49E', '#EE7B91', '#E85285', '#BE408C', '#BE408C'],
@@ -62,14 +26,6 @@ const TimeSeriesWithAuxiliaryView = ({ views = {}, options = {}, title, descript
 
   const { registerTheme } = G2;
   registerTheme('custom-theme', theme);
-
-  useEffect(() => {
-    if (plot) {
-      plot.on('legend-item:click', (...args: any) => {
-        console.log('CLICKED!', ...args);
-      });
-    }
-  }, [plot]);
 
   useEffect(() => {
     const chartOptions = {};
@@ -117,35 +73,12 @@ const TimeSeriesWithAuxiliaryView = ({ views = {}, options = {}, title, descript
   }, [views, options]);
 
   return (
-    <div className={styles.container}>
-      <div className={styles.header}>
-        <h2 className={styles.title}>{title}</h2>
-        <h4 className={styles.subtitle}>{description}</h4>
-      </div>
-      <DateRangePickerMenu onChange={handleDatesChanged} />
-      <Mix
-        {...chartConfig}
-        onReady={(plt: any) => {
-          plt.chart.theme('custom-theme');
-          setPlot(plt);
-        }}
-      />
-      <div className={styles.legend}>
-        {legendItems.current.map((legend: any) => (
-          <div
-            key={legend.link}
-            className={styles.legendItemActive}
-            style={{ borderColor: legend.color }}
-            onClick={(e) => {
-              handleLegendClick(e.target, legend);
-            }}>
-            <a href={legend.link} onClick={(e) => e.stopPropagation()}>
-              {legend.text}
-            </a>
-          </div>
-        ))}
-      </div>
-    </div>
+    <Mix
+      {...chartConfig}
+      onReady={(plt: any) => {
+        plt.chart.theme('custom-theme');
+      }}
+    />
   );
 };
 
