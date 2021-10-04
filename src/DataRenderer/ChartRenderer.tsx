9b7aa6b31d4cd32a8193b35887cb40279cf85605
Dmitry Oshkalo <dmitry.oshkalo@gmail.com>
2021-10-04 11:18:30 +0300

    Enable chart rendering without waiting for data

--- src/DataRenderer/ChartRenderer.tsx
+++ src/DataRenderer/ChartRenderer.tsx
@@ -7,19 +7,13 @@
  *
  * SPDX-License-Identifier: GPL-3.0-only
  ********************************************************************************/
-import {
-  IViewDescrElement,
-  MstContext,
-  mstJsonLdIds,
-  processViewKindOverride,
-  RenderProps,
-} from '@agentlab/ldkg-ui-react';
+import { MstContext, mstJsonLdIds, processViewKindOverride, RenderProps } from '@agentlab/ldkg-ui-react';
 import { Spin } from 'antd';
 import { observer } from 'mobx-react-lite';
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext } from 'react';
 import Chart from './Chart';
 import { createComponent } from './mappers/components';
-import { buildViewConfig, ElementDataProvider, findElementsRecursive, MappingsProvider, SchemaProvider } from './utils';
+import { buildViewConfig, ElementDataProvider, MappingsProvider, SchemaProvider } from './utils';
 
 export const ChartRenderer = observer<RenderProps>((props): JSX.Element => {
   const { store } = useContext(MstContext);
@@ -34,101 +28,42 @@ export const ChartRenderer = observer<RenderProps>((props): JSX.Element => {
     viewKindElement: mstJsonLdIds(viewKindElement),
     viewDescrElement: mstJsonLdIds(viewDescrElement),
   });
-  const [views, setViews] = useState<any>([]);
-  const [viewConfig, setViewConfig] = useState<any | undefined>();
-  const [isDataReady, setIsDataReady] = useState<boolean>(false);
+  const elementDataProvider: ElementDataProvider = (element) => {
+    const resultsScope = element.resultsScope;
+    let dataObs = store.getColl(resultsScope);
+    dataObs = dataObs.dataJs;
+    return dataObs;
+  };
+  const schemaProvider: SchemaProvider = (constraint) =>
+    typeof constraint.schema === 'string' ? store.schemas.get(constraint.schema) : constraint.schema;
 
-  const resultsScope = viewDescrElement?.resultsScope;
-  let lastSynced;
-  if (resultsScope) {
-    const coll = store.getColl(resultsScope);
-    lastSynced = coll?.lastSynced;
-  }
+  const mappingsProvider: MappingsProvider = (elementType) =>
+    (viewKindElement as any).mappings ? (viewKindElement as any).mappings[elementType] : undefined;
 
-  //TODO: It should be refactored to support lazy loading and should pick up data change with a help of observer HOC
-  // today data change is lost into the deeper data map functions
-  useEffect(() => {
-    if (isDataReady) {
-      console.log('ChartRenderer - data = OK');
-      if (viewDescrElement && viewKindElement) {
-        const elementDataProvider: ElementDataProvider = (element) => {
-          const resultsScope = element.resultsScope;
-          let dataObs = store.getColl(resultsScope);
-          dataObs = dataObs.dataJs;
-          return dataObs;
-        };
-        const schemaProvider: SchemaProvider = (constraint) =>
-          typeof constraint.schema === 'string' ? store.schemas.get(constraint.schema) : constraint.schema;
+  const config = buildViewConfig(
+    viewDescrElement,
+    viewDescr,
+    viewKind,
+    elementDataProvider,
+    schemaProvider,
+    mappingsProvider,
+  );
+  console.log('setViewConfig', config);
 
-        const mappingsProvider: MappingsProvider = (elementType) =>
-          (viewKindElement as any).mappings ? (viewKindElement as any).mappings[elementType] : undefined;
-
-        const config = buildViewConfig(
-          viewDescrElement,
-          viewDescr,
-          viewKind,
-          elementDataProvider,
-          schemaProvider,
-          mappingsProvider,
-        );
-        console.log('setViewConfig', config);
-        setViewConfig(config);
-      } else {
-        console.log('did not setViewConfig', { viewConfig, viewDescrElement, viewKindElement });
-      }
-    } else {
-      console.log('ChartRenderer - data != OK');
-    }
-  }, [isDataReady, store, viewDescrElement, viewKindElement, lastSynced]);
-
-  // Data & Mapping
-  useEffect(() => {
-    async function loadViews(viewDescrElement: IViewDescrElement) {
-      const chartConfig = {
-        title: viewDescrElement?.title,
-        description: viewDescrElement?.description,
-        options: viewDescrElement?.options,
-        views: [viewConfig],
-      };
-      const dataViewComponent = await createComponent(viewKindElement['@type']);
-      const memoizedDataViewComponent = React.memo(dataViewComponent);
-      console.log('call setViews');
-      setViews([{ View: memoizedDataViewComponent, key: viewDescrElement['@id'], config: chartConfig }]);
-    }
-    if (viewDescrElement && viewKindElement && viewConfig) {
-      console.log('call loadViews');
-      loadViews(viewDescrElement);
-    } else {
-      console.log('did not call loadViews', { viewDescrElement, viewKindElement, viewConfig });
-    }
-  }, [store, viewConfig, viewDescrElement, viewKindElement]);
-
-  const elements = findElementsRecursive(viewDescr.elements, (el: IViewDescrElement) => el.resultsScope !== undefined);
-  let isAllNotEmpty = true;
-  elements.forEach((e: any) => {
-    if (!store.getColl(e.resultsScope) || store.getColl(e.resultsScope)?.data.length <= 0) {
-      isAllNotEmpty = false;
-      console.log('ChartRenderer - data - empty', e.resultsScope);
-    }
-  });
-  if (isAllNotEmpty) console.log('ChartRenderer - data = OK 1', { isAllNotEmpty, isDataReady });
-  if (isAllNotEmpty !== isDataReady) {
-    console.log('setIsDataReady', { isAllNotEmpty, isDataReady });
-    setIsDataReady(isAllNotEmpty);
-  }
-  if (!isAllNotEmpty) return <Spin />;
+  const dataViewComponent = createComponent(viewKindElement['@type']);
+  const View = React.memo(dataViewComponent);
+  const key = viewDescrElement['@id'];
+  const options = {
+    ...viewDescrElement?.options,
+    title: viewDescrElement?.title,
+    description: viewDescrElement?.description,
+  };
 
   return (
     <React.Suspense fallback={<Spin />}>
-      {views.map((item: { View: any; key: any; config: any }) => {
-        const { View, key, config } = item;
-        const { options, views } = config;
-        return (
-          <Chart key={key} {...config}>
-            <View options={options} config={views[0]} />
-          </Chart>
-        );
-      })}
+      <Chart key={key} options={options}>
+        <View options={options} config={config} />
+      </Chart>
     </React.Suspense>
   );
 });
