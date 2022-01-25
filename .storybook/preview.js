239f11e753056cd03d2d1e669330cc3902a5e083
Alexey Ivanov <amivanoff@gmail.com>
2022-01-25 15:16:36 +0300

    [add] Antd and moment locale support

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,7 +1,16 @@
+import { ConfigProvider } from 'antd';
 import 'antd/dist/antd.less';
+import ruRu from 'antd/es/locale/ru_RU';
+import moment from 'moment';
+import React from 'react';
 
 // https://storybook.js.org/docs/react/writing-stories/parameters#global-parameters
 export const parameters = {
   // https://storybook.js.org/docs/react/essentials/actions#automatically-matching-args
   actions: { argTypesRegex: '^on.*' },
 };
+
+export const decorators = [(Story) => <ConfigProvider locale={ruRu}>{Story()}</ConfigProvider>];
+
+moment.locale('ru');
+moment.defaultFormat = 'LLL';
